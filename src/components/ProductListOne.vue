0e6f12d11fbc4d78275930246aad0d7c71dc3a09
Eyakub <eyakubsorkar@gmail.com>
2020-01-22 16:55:09 +0600

    feat: mapping actions & getter docs: importing mapAction, mapGetter can help us to import multiple getter and actions

--- src/components/ProductListOne.vue
+++ src/components/ProductListOne.vue
@@ -11,30 +11,38 @@
     </div>
 </template>
 <script>
+import {mapActions} from 'vuex';
+import {mapGetters} from 'vuex';
+
 export default {
     computed: {
         products() {
             return this.$store.state.products;
         },
-        saleProducts() {
-            return this.$store.getters.saleProducts;
-        }
+        // for multiple getters
+        ...mapGetters([
+          "saleProducts",
+          "yourNext",
+        ])
     },
     methods: {
-        reducePrice(amount) {
-            // commit a mutations that will observing the state will update automatically
-            // by this way we can tract it on vueDevTools
-            //this.$store.commit("reducePrice");
+        // reducePrice(amount) {
+        //     // commit a mutations that will observing the state will update automatically
+        //     // by this way we can tract it on vueDevTools
+        //     //this.$store.commit("reducePrice");
 
-            // dispatch an action is good practice
-            // instead commit directly
-            /* $dispatch is always used from your methods in routes/components. 
-               It sends a message to Vuex store to perform some action. 
-               The action can be performed any time after the current tick 
-               so that it will not affect the frontend performance. 
-               The commit is done from within an action.*/
-            this.$store.dispatch('reducePrice', amount)
-        }
+        //     // dispatch an action is good practice
+        //     // instead commit directly
+        //     /* $dispatch is always used from your methods in routes/components. 
+        //        It sends a message to Vuex store to perform some action. 
+        //        The action can be performed any time after the current tick 
+        //        so that it will not affect the frontend performance. 
+        //        The commit is done from within an action.*/
+        //     this.$store.dispatch('reducePrice', amount)
+        // }
+        ...mapActions([
+          "reducePrice",
+        ])
     }
 };
 </script>
